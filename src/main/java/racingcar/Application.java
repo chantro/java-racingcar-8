498c5229e69a7f7f3f72c135fd98c72e34f4ed53
chantro <sophia2359@gmail.com>
2025-10-27 14:00:37 +0900

    feat(app): 메인 실행
    InputView, OutputView, CarRace 통합 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import racingcar.domain.Car;
+import racingcar.domain.CarRace;
+import racingcar.policy.MovePolicy;
+import racingcar.policy.RandomMoveForwardPolicy;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        //입력값 받기
+        InputView inputView = new InputView();
+        List<Car> cars = inputView.readCarNames().stream().map(Car::new).toList();
+        int rounds = inputView.readRounds();
+
+        //move 정책 생성
+        int threshold = 4;
+        MovePolicy policy = new RandomMoveForwardPolicy(threshold);
+        OutputView outputView = new OutputView();
+        CarRace race = new CarRace(cars, rounds, policy, outputView);
+
+        //경기 시작
+        race.play();
     }
 }
