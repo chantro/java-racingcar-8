97c3c091f9902da2329dbcdd54774013d6b93d4b
chantro <sophia2359@gmail.com>
2025-10-27 14:04:08 +0900

    fix(view, policy): 잘못된 입력 시 예외처리
    1. 이름 예외
    - 공백 트림, 빈 토큰 금지, 중복 금지, 5자 초과 금지.
    2. 시도 횟수(round) 예외
    - 양의 int 형 정수만 허용.
    3. 정책 예외
    - threshold 범위는 0~9로 제한.

--- src/main/java/racingcar/policy/RandomMoveForwardPolicy.java
+++ src/main/java/racingcar/policy/RandomMoveForwardPolicy.java
@@ -6,6 +6,9 @@ public final class RandomMoveForwardPolicy implements MovePolicy {
   private final int threshold;
 
   public RandomMoveForwardPolicy(int threshold) {
+    if (threshold < 0 || threshold > 9) {
+      throw new IllegalArgumentException("threshold는 0~9 사이의 값이어야 합니다.");
+    }
     this.threshold = threshold;
   }
 
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,9 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public final class InputView {
   private final String readCarNamesText = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -12,15 +14,43 @@ public final class InputView {
 
   public List<String> readCarNames() {
     System.out.println(readCarNamesText);
-    List<String> names = Arrays.stream(Console.readLine().split(regex))
+    List<String> names = Arrays.stream(Console.readLine().split(regex, -1))
             .map(String::trim)
             .toList();
+    validateNames(names);
     return names;
   }
 
   public int readRounds() {
     System.out.println(readRoundsText);
     String rounds_string = Console.readLine().trim();
-    return Integer.parseInt(rounds_string);
+    return validateRounds(rounds_string);
+  }
+
+  private int validateRounds(String rounds_string) {
+    try {
+      int rounds = Integer.parseInt(rounds_string);
+      if (rounds <= 0) {
+        throw new IllegalArgumentException("round는 양수여야 합니다.");
+      }
+      return rounds;
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("round는 int형 정수여야 합니다.");
+    }
+  }
+
+  private void validateNames(List<String> names) {
+    Set<String> existNames = new HashSet<>();
+    for (String name : names) {
+      if (name.isEmpty()) {
+        throw new IllegalArgumentException("처음 또는 쉼표(,) 다음에는 이름이 한 글자 이상 있어야 합니다.");
+      }
+      if (name.length() > 5) {
+        throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다");
+      }
+      if (!existNames.add(name)) {
+        throw new IllegalArgumentException("중복된 자동차 이름이 있습니다");
+      }
+    }
   }
 }
